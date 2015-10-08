06f1af5bc76b29af28072aa3bc1242e285a00541
PrimeAC <afonso.caetano@outlook.com>
2015-10-08 17:58:09 +0100

    invalid argument fix

--- par-shell.c
+++ par-shell.c
@@ -40,69 +40,73 @@ int main(int argc, char* argv[]){
   argVector = (char**) malloc(NARGUMENTOS*sizeof(char*));
   
   while(1){
-    readLineArguments(argVector, NARGUMENTOS);
-  
+    if(readLineArguments(argVector, NARGUMENTOS)>0){
+    
+      if(strcmp(argVector[0], "exit")==0){
 
-    if(strcmp(argVector[0], "exit")==0){
+        lst_print(list);
 
-      lst_print(list);
+        for(i=0; i<=num_filhos+1; i++){
 
-      for(i=0; i<=num_filhos+1; i++){
+          wait(&status);
+          num_filhos--;
 
-        wait(&status);
-        num_filhos--;
-
-      }
-      lst_destroy(list);
-      free(argVector[0]);
-      free(argVector);
-      exit(0);
-
-      /*exit routine wait exit*/
-      
-    }
-
-    else {
-      
-      PID=fork();
-      if(PID<0){
-
-        perror("");
-
-        exit(EXIT_SUCCESS);
-
-      }
-
-      if(PID==0){
-        /* filho*/        
-        if(execv(argVector[0],argVector)<0){
-
-        	perror("");
-          free(argVector[0]);
-          free(argVector);
-
-        	exit(EXIT_FAILURE);
-       
         }
-      }
-      else if(wait(&status) != PID){ /*nao esta a chegar aqui*/
+        lst_destroy(list);
+        free(argVector[0]);
+        free(argVector);
+        exit(0);
 
-        perror("um sinal interrompeu o wait\t");
-      }        
-      
-
-      else{
-        /* pai*/
-        wait(&status);
+        /*exit routine wait exit*/
         
       }
-      /*pathname routine fork exec*/
-      if(WIFEXITED(status)==1){ 
-        insert_new_process(list,PID,WEXITSTATUS(status));
-        num_filhos++;
-      }
-      free(argVector[0]);
-    }  
+
+      else {
+        
+        PID=fork();
+        if(PID<0){
+
+          perror("");
+
+          exit(EXIT_SUCCESS);
+
+        }
+
+        if(PID==0){
+          /* filho*/
+          
+          if(execv(argVector[0],argVector)<0){
+
+          	perror("");
+            free(argVector[0]);
+            free(argVector);
+
+          	exit(EXIT_FAILURE);
+         
+          }
+        }
+        else if(wait(&status) != PID){ /*nao esta a chegar aqui*/
+
+          perror("um sinal interrompeu o wait\t");
+        }        
+        
+
+        else{
+          /* pai*/
+          wait(&status);
+          
+        }
+        /*pathname routine fork exec*/
+        if(WIFEXITED(status)==1){ 
+          insert_new_process(list,PID,WEXITSTATUS(status));
+          num_filhos++;
+        }
+        free(argVector[0]);
+      }  
+    }
+    else{
+      printf("Por favor insira um argumento válido!\n");
+    }
   }
   
 
