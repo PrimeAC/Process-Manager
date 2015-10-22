92fc47ac39825181a670eca59c3bf42eedae3f8d
PrimeAC <afonso.caetano@outlook.com>
2015-10-22 17:41:04 +0100

    add threads and different input

--- par-shell.c
+++ par-shell.c
@@ -25,28 +25,45 @@ Sistemas Operativos
 #include "commandlinereader.h"
 #include "list.h"
 #include <sys/wait.h>
+
+void tarefaMonitora(){  
+
+  while (1){
+    if (num_filhos==0) 
+
+      sleep(1);
+    else
+      wait();
+      update_terminated_process(list, PID, endtime)
+
+}
+
 /*
 Main Program
 */
-
-
 int main(int argc, char* argv[]){
 
   char **argVector;
-  int PID, num_filhos=0, status=0, i;
-
-  list_t* list = lst_new(); /*cria uma lista onde é guardado o pid e o status dos processos*/
+  int PID, num_filhos=0, status=0, i, 
+  pthread_t = tid[1]
+  time_t 
+  list_t* list = lst_new(); /*cria uma lista onde é guardado o PID e o status dos processos*/
+  int 
 
   argVector = (char**) malloc(NARGUMENTOS*sizeof(char*));
-  
+
+
   while(1){
+
+    err = pthread_create(tid,0,tarefaMonitora,0)
+
     if(readLineArguments(argVector, NARGUMENTOS)>0){ /*verifica se o utilizador escreveu algo */
       
       if(strcmp(argVector[0], "exit")==0){
         
-        
+        pthread_join(tid[1],NULL)
        
-        for(i=0; i<num_filhos; i++){
+        for(;num_filhos>0;num_filhos--){
          
           PID=wait(&status);/*aguarda que os processos filhos terminem*/ 
 
@@ -71,6 +88,8 @@ int main(int argc, char* argv[]){
       else {
         
         PID=fork();/*guarda na variavel PID o resultado da funçao fork*/
+
+        insert_new_process(list_t *list, int PID, time_t starttime)
         
         if(PID<0){  /*caso ocorra erro na criacao do processo filho*/
           perror("");
@@ -88,7 +107,7 @@ int main(int argc, char* argv[]){
          
           }
         }
-
+        insert_new_process(list, PID, tstarttime)/*lista do lab 1 - mudar o nome*/
         num_filhos++;
         
         free(argVector[0]);
